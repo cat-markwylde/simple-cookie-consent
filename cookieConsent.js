f9aa3456b94d9b8374da8c693f9377336ed31f95
Mark Wylde <mark.wylde@checkatrade.com>
2022-05-24 09:31:42 +0100

    Initial commit

---
+++ cookieConsent.js
@@ -0,0 +1,29 @@
+let cookieQueue = [];
+const originalCookieSetter = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie').set;
+Object.defineProperty(document, 'cookie', {
+  set: function(value) {
+    if (localStorage.getItem('cookieConsent')) {
+      originalCookieSetter.call(document, value);
+      return;
+    }
+
+    cookieQueue.push(value);
+  }
+});
+
+window.cookieConsent = (value) => {
+  localStorage.setItem('cookieConsent', true)
+
+  if (localStorage.getItem('cookieConsent')) {
+    cookieQueue.forEach(item => {
+      originalCookieSetter.call(document, item);
+    });
+    cookieQueue = [];
+  }
+}
+
+function addStyle(styleString) {
+  const style = document.createElement('style');
+  style.textContent = styleString;
+  document.head.append(style);
+}
