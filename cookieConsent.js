4b6ac5862602eb82e8ad7dd0f61da599bd46e495
Mark Wylde <mark.wylde@checkatrade.com>
2022-05-24 09:46:21 +0100

    Add remove consent button

--- cookieConsent.js
+++ cookieConsent.js
@@ -1,5 +1,6 @@
 let cookieQueue = [];
 const originalCookieSetter = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie').set;
+const originalCookieGetter = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie').get;
 Object.defineProperty(document, 'cookie', {
   set: function(value) {
     if (localStorage.getItem('cookieConsent') === 'true') {
@@ -19,6 +20,22 @@ window.cookieConsent = (value) => {
       originalCookieSetter.call(document, item);
     });
     cookieQueue = [];
+  } else {
+    deleteAllCookies();
+    localStorage.clear();
+    window.location.reload();
+  }
+}
+
+function deleteAllCookies() {
+  const originalCookie = originalCookieGetter.call(document);
+  var cookies = originalCookie.split(";");
+
+  for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      originalCookieSetter.call(document, name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT");
   }
 }
 
